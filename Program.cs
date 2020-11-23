893a2fea59dbc41523dadf8628332bf8e62e0a23
“teji1998” <tejasvinirpk@gmail.com>
2020-11-23 10:59:50 +0530

    Added:Code for position of player and number of times dice rolled

--- Program.cs
+++ Program.cs
@@ -13,25 +13,28 @@ namespace snakeAndLadderProblem
             const int ladderOption = 1;
             const int snakeOption = 2;
             const int playerOne = 1;
-
-            //variables
+            //variable
             int positionOne = 0;
-
-            //using while loop 
+            int diceRollCount = 0;
+            //while loop to check the condition of position is greater than zero
             while (positionOne < 100)
             {
                 Random random = new Random();
                 int dieRoll = random.Next(1, 7);
+                diceRollCount++;
                 int options = random.Next(0, 3);
+                Console.WriteLine("Position: " + positionOne);
                 switch (options)
                 {
                     case noPlayOption:
+                        Console.WriteLine("Position: " + positionOne);
                         break;
                     case ladderOption:
                         if (positionOne + dieRoll <= 100)
                         {
                             positionOne = positionOne + dieRoll;
                         }
+                        Console.WriteLine("Position: " + positionOne);
                         break;
                     case snakeOption:
                         positionOne = positionOne - dieRoll;
@@ -39,10 +42,11 @@ namespace snakeAndLadderProblem
                         {
                             positionOne = 0;
                         }
+                        Console.WriteLine("Position: " + positionOne);
                         break;
                 }
             }
-            Console.WriteLine("Player Wins with Position :" + positionOne);
+            Console.WriteLine("Player Wins with Die Roll :" + diceRollCount);
 
         }
     }
