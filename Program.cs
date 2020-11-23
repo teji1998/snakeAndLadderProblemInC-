699289cc8c73436ae43364c448907460447f9206
“teji1998” <tejasvinirpk@gmail.com>
2020-11-23 10:23:08 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace snakeAndLadderProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder problem!");
+        }
+    }
+}
