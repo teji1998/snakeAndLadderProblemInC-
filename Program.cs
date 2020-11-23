a3c770d5b4e2091a66739c5c3f8274b1ee1d76b0
“teji1998” <tejasvinirpk@gmail.com>
2020-11-23 10:33:22 +0530

    Added:Code to roll the die

--- Program.cs
+++ Program.cs
@@ -11,6 +11,13 @@ namespace snakeAndLadderProblem
             //constants
             const int playerOne = 1;
             int positionOne = 0;
+
+            Random random = new Random();
+
+            //returns a random no between 1 and 7
+            int dieRoll = random.Next(1, 7); 
+
+            Console.WriteLine("The number on die when rolled is : " + dieRoll);
         }
     }
 }
